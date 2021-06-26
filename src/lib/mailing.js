6e56070bb16803a10b498d515a290fa29ace042e
Lluc <60989904+LukeP13@users.noreply.github.com>
2021-06-26 16:58:22 +0200

    Update mailing.js

--- src/lib/mailing.js
+++ src/lib/mailing.js
@@ -2,15 +2,6 @@ const app = require("express")();
 const mailer = require("express-mailer");
 
 mailer.extend(app, {
-  from: "no-reply@bikeInspec.com",
-  host: "smtp.gmail.com", // hostname
-  secureConnection: true, // use SSL
-  port: 465, // port for secure SMTP
-  transportMethod: "SMTP", // default is SMTP.
-  auth: {
-    user: "bikeInspectioner@gmail.com", // Your Email
-    pass: "Kawa25Project", // Your Password
-  },
 });
 
 app.engine("pug", require("pug").__express);
